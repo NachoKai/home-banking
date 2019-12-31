bb864332f608f191d5f484f752c753b9dca878a0
NachoKai <47578311+NachoKai@users.noreply.github.com>
2019-12-31 18:54:40 -0300

    Update test.js

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,6 +1,6 @@
 const URL = 'https://nachokai.github.io/home-banking/';
 
-describe('My First Test', function () {
+describe('Home-Banking Test', function () {
 
     it('finds the content "OK" and click it and fail', function () {
         cy.visit(URL)
@@ -13,6 +13,48 @@ describe('My First Test', function () {
         cy.visit(URL)
         cy.get('input').first().type('1234')
         cy.contains('OK').click()
+        cy.wait(500)
+        cy.get('[class^=swal2-actions]').click({
+            multiple: true
+        })
+    })
+
+    it('extracts $500', function () {
+        cy.wait(500)
         cy.get('#saldo-cuenta').contains('$10000')
+        cy.get('.white-container').first()
+        cy.get('.links').contains('Extraer dinero').click()
+        cy.get('input').first().type('500')
+        cy.get('[class^=swal2-confirm]').click({
+            multiple: true
+        })
+    })
+
+    it('deposits $500', function () {
+        cy.wait(500)
+        cy.get('[class^=swal2-confirm]').click({
+            multiple: true
+        })
+        cy.get('#saldo-cuenta').contains('$9500')
+        cy.get('.white-container').first()
+        cy.get('.links').contains('Depositar dinero').click()
+        cy.get('input').first().type('500')
+        cy.get('[class^=swal2-confirm]').click({
+            multiple: true
+        })
+        cy.wait(500)
+        cy.get('[class^=swal2-confirm]').click({
+            multiple: true
+        })
+    })
+
+    it('pay services', function () {
+        cy.wait(500)
+        cy.get('#saldo-cuenta').contains('$10000')
+        cy.get('.white-container').first()
+        cy.get('.links').contains('Pagar servicios').click()
+        cy.window().then((win) => {
+            cy.stub(win, 'prompt').returns('1')
+        })
     })
 })
